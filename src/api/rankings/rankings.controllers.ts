5eb8b5bacda5f196d351f4d157c541e910d73847
Jaw <58354193+wajeht@users.noreply.github.com>
2022-10-14 05:19:25 -0400

    feat: added obj header to rankings

--- src/api/rankings/rankings.controllers.ts
+++ src/api/rankings/rankings.controllers.ts
@@ -6,10 +6,42 @@ import axios from '../../utils/axios';
 
 export async function getRankings(req: Request, res: Response): Promise<any> {
   const x = `?start=0&end=100&lang=en&units=lbs`;
-  const data = await (await axios.get(RANKINGS_URL + x)).data;
+  const { rows, total_length } = await (await axios.get(RANKINGS_URL + x)).data;
+
+  const data = rows.map((r: any) => {
+    return {
+      id: r[0],
+      rank: r[1],
+      full_name: r[2],
+      username: r[3],
+      instagram: r[4],
+      username_color: r[5],
+      country: r[6],
+      location: r[7],
+      fed: r[8],
+      date: r[9],
+      country_two: r[10],
+      state: r[11],
+      meet_code: r[12],
+      sex: r[13],
+      equip: r[14],
+      age: parseInt(r[15]),
+      open: r[16],
+      body_weight: parseFloat(r[17]),
+      weight_class: parseFloat(r[18]),
+      squat: parseFloat(r[19]),
+      bench: parseFloat(r[20]),
+      deadlift: parseFloat(r[21]),
+      total: parseFloat(r[22]),
+      dots: parseFloat(r[23]),
+    };
+  });
 
   return res.status(StatusCodes.OK).json({
     status: 'ok',
-    data,
+    data: {
+      total: total_length,
+      rows: data,
+    },
   });
 }
