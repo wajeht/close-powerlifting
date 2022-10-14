e77a0c6e4783bd8d3d0ae53003724f1a1474a5da
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-14 18:29:23 -0400

    feat: updated error handling response and added env

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,21 +1,41 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { ENV } from './config/constants';
 
 export function healthCheckHandler(req: Request, res: Response, next: NextFunction) {
   return res.status(StatusCodes.OK).json({
+    status: 'success',
+    request_url: req.originalUrl,
     message: 'ok',
-    date: new Date(),
+    cache: req.query?.cache,
+    data: [
+      {
+        date: new Date(),
+      },
+    ],
   });
 }
 
 export function notFoundHandler(req: Request, res: Response, next: NextFunction) {
   return res.status(StatusCodes.NOT_FOUND).json({
-    message: 'not found!',
+    status: 'fail',
+    request_url: req.originalUrl,
+    message: 'The resource does not exist!',
+    cache: req.query?.cache,
+    data: [],
   });
 }
 
 export function serverErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
+    status: 'fail',
+    request_url: req.originalUrl,
+    errors: err?.errors,
+    message:
+      ENV === 'development'
+        ? err.stack
+        : 'The server encountered an internal error or misconfiguration and was unable to complete your request.',
+    cache: req.query?.cache,
+    data: [],
   });
 }
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,7 +4,6 @@ import path from 'path';
 dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 
 export const PORT = process.env.PORT;
-
 export const RANKINGS_URL = process.env.RANKINGS_URL;
-
 export const AXIOS_HOST = process.env.AXIOS_HOST;
+export const ENV = process.env.ENV;
