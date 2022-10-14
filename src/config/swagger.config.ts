4959e260f95b6b615c17436a6848c209644a3aa0
Jaw <58354193+wajeht@users.noreply.github.com>
2022-10-14 05:43:20 -0400

    feat: update swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import { PORT } from '../config/constants';
+import pkg from '../../package.json';
+
 export default {
   info: {
     title: 'close-powerlifting',
@@ -13,13 +15,11 @@ export default {
       name: 'MIT',
       url: 'https://www.gnu.org/licenses/gpl-3.0.en.html',
     },
-    version: '0.0.1',
+    version: pkg.version,
   },
   baseDir: path.resolve(path.join(process.cwd(), 'src', 'api')),
-  filesPattern: ['./**/*.router.ts', '*.ts', './**/*.routes.ts', 'app.js'],
+  filesPattern: ['./**/*.router.ts', './**/*.routes.ts'],
   swaggerUIPath: '/docs/api',
   exposeSwaggerUI: true,
   notRequiredAsNullable: false,
-  swaggerUiOptions: {},
-  multiple: {},
 };
