6dfd39932b4acbe09b47dc8ac37a1e7698e0eb3b
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-14 18:24:18 -0400

    feat: escaped axios host to .env file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,3 +6,5 @@ dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 export const PORT = process.env.PORT;
 
 export const RANKINGS_URL = process.env.RANKINGS_URL;
+
+export const AXIOS_HOST = process.env.AXIOS_HOST;
