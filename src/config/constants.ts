34f48394e8c34ef38381ce8cef43ea9efedec60d
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-18 19:30:45 -0400

    fix: SESSION_SECRET was not named correctly

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,5 +6,5 @@ dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 export const PORT = process.env.PORT as unknown as number;
 export const API_URL = process.env.API_URL as unknown as string;
 export const BASE_URL = process.env.BASE_URL as unknown as string;
-export const SESSION_SECRET = process.env.COOKIE_SECRET as unknown as string;
+export const SESSION_SECRET = process.env.SESSION_SECRET as unknown as string;
 export const ENV = process.env.ENV as unknown as string;
