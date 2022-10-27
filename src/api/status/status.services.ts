846107bf246f059b0e6c4e1180e97d0a495389fa
wajeht <58354193+wajeht@users.noreply.github.com>
2022-10-27 19:46:55 -0400

    refactor: /api/status to only grab if cache was true

--- src/api/status/status.services.ts
+++ src/api/status/status.services.ts
@@ -7,22 +7,35 @@ import { getStatusType } from './status.validations';
 import Axios from '../../utils/axios';
 const api = new Axios(true).instance();
 
+export async function fetchStatus() {
+  try {
+    const html = await (await api.get('/status')).data;
+    const dom = new JSDOM(html);
+    const div = dom.window.document.getElementsByClassName('text-content');
+    return {
+      server_version: stripHTML(div[0].children[2].innerHTML),
+      meets: div[0].childNodes[8].textContent?.toString(),
+      federations: tableToJson(div[0].children[5]),
+    };
+  } catch (e) {
+    return null;
+  }
+}
+
 export async function getStatus({ cache = true }: getStatusType) {
   try {
+    if (cache === false) {
+      return {
+        data: await fetchStatus(),
+        cache,
+      };
+    }
+
     // @ts-ignore
     let data = JSON.parse(await redis.get(`close-powerlifting-status`));
 
     if (data === null) {
-      const html = await (await api.get('/status')).data;
-      const dom = new JSDOM(html);
-      const div = dom.window.document.getElementsByClassName('text-content');
-
-      data = {
-        server_version: stripHTML(div[0].children[2].innerHTML),
-        meets: div[0].childNodes[8].textContent?.toString(),
-        federations: tableToJson(div[0].children[5]),
-      };
-
+      data = await fetchStatus();
       // @ts-ignore
       const status = await redis.set(`close-powerlifting-status`, JSON.stringify(data));
     }
