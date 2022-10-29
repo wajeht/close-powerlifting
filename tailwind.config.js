c7c5d27f30d18d5a19c52be3d25df1b644932c26
Jaw <58354193+wajeht@users.noreply.github.com>
2022-10-29 15:06:43 -0400

    fix: responsive header/footer for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/views/**/*.{html,js}'],
   theme: {
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [
