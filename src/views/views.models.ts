f55e090900b344e4e94e891cb5c1366eaed0fd25
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-07 19:34:31 -0600

    feat: added admin field to user model

--- src/views/views.models.ts
+++ src/views/views.models.ts
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     require: false,
   },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
   deleted: {
     type: Boolean,
     require: true,
