62918f50d8de4a7abc9f8bc500fda2ee94972684
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-07 21:49:17 -0600

    feat: attach admin user on server start

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as db from './utils/db';
 import * as crons from './utils/crons';
+import * as admin from './utils/admin-user'
 import app from './app';
 import { PORT } from './config/constants';
 import logger from './utils/logger';
@@ -7,6 +8,7 @@ import logger from './utils/logger';
 app.listen(PORT, async () => {
   await db.init();
   await crons.init();
+  await admin.init();
 
-  logger.info(`app was started on http://localhost:${PORT}`);
+  logger.info(`**** app was started on http://localhost:${PORT} ****`);
 });
---
+++ src/utils/admin-user.ts
@@ -0,0 +1,92 @@
+import bcrypt from 'bcryptjs';
+import { User } from '../views/views.models';
+import { ADMIN, PASSWORD_SALT, JWT_SECRET, EMAIL } from '../config/constants';
+import logger from './logger';
+import jwt from 'jsonwebtoken';
+import { faker } from '@faker-js/faker';
+import { hashKey } from '../utils/helpers';
+import mail from './mail';
+
+export async function init() {
+  try {
+    logger.info('**** attaching admin user ****');
+
+    const found = await User.findOne({ email: ADMIN.EMAIL });
+
+    if (!found) {
+      const password = faker.internet.password(50);
+      const hashedPassword = await bcrypt.hash(password, parseInt(PASSWORD_SALT!));
+      const { key: token } = await hashKey();
+
+      const created = await User.create({
+        email: ADMIN.EMAIL,
+        name: ADMIN.NAME,
+        admin: true,
+        password: hashedPassword,
+        verification_token: token,
+        verified: true,
+        verified_at: new Date().toISOString(),
+      });
+
+      const apiKey = jwt.sign(
+        {
+          id: created.id,
+          name: created.name,
+          email: created.email,
+        },
+        JWT_SECRET!,
+        {
+          issuer: 'Close Powerlifting',
+        },
+      );
+
+      const hashedApiKey = await bcrypt.hash(apiKey, parseInt(PASSWORD_SALT!));
+
+      let verified = await User.findOneAndUpdate(
+        {
+          email: created.email,
+        },
+        {
+          $set: {
+            key: hashedApiKey,
+          },
+        },
+        {
+          returnOriginal: false,
+        },
+      );
+
+      const info = await mail.sendMail({
+        from: `"Close Powerlifting" <${EMAIL.AUTH_EMAIL}>`,
+        to: created.email,
+        subject: 'API Key for Close Powerlifting',
+        html: `
+          <div>
+            <p>Hi ${verified!.name},</p>
+            <br>
+
+            <p>Here below is your API key and admin password to access Close Powerlifting!</p>
+
+            <br>
+            <br>
+            <p>Admin password</p>
+            <div style="background: #171717; text-decoration: none; color: white; display:inline-block; padding: 5px;">${password}</div>
+            <br>
+            <p>API Key:</p>
+            <div style="background: #171717; text-decoration: none; color: white; display:inline-block; padding: 5px;">${apiKey}</div>
+            <br>
+            <br>
+
+            <br>
+            <p>Welcome to the Close Powerlifting,</p>
+            <p>Let's make all kinds of gains. All kindszzzz.!</p>
+          </div>
+          `,
+      });
+
+      logger.info(`**** admin user: ${ADMIN.EMAIL} - ${ADMIN.EMAIL} has been attached! ****`);
+    }
+  } catch (e) {
+    logger.error(e);
+  }
+}
