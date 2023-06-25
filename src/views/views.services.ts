ed415ad0d0dcb726ccc1103b50170aa671436033
wajeht <58354193+wajeht@users.noreply.github.com>
2023-06-24 23:52:20 -0500

    feat: cache status page api call for 60 min

--- src/views/views.services.ts
+++ src/views/views.services.ts
@@ -6,6 +6,8 @@ import { EMAIL, PASSWORD_SALT } from '../config/constants';
 import { generateAPIKey } from '../utils/helpers';
 import logger from '../utils/logger';
 import mail from '../utils/mail';
+// @ts-ignore
+import redis from '../utils/redis';
 import adminNewAPIKeyHTML from '../utils/templates/admin-new-api-key';
 import newAPIKeyHTML from '../utils/templates/new-api-key';
 import verifyEmailHTML from '../utils/templates/verify-email';
@@ -111,41 +113,60 @@ export async function sendWelcomeEmail(userParams: UserParams): Promise<void> {
 }
 
 export async function getAPIStatus({ X_API_KEY, url }: { X_API_KEY: string; url: string }) {
-  const fetch = axios.create({
-    baseURL: url,
-    headers: {
-      authorization: `Bearer ${X_API_KEY}`,
-    },
-  });
+  const fetchStatus = async () => {
+    const fetch = axios.create({
+      baseURL: url,
+      headers: {
+        authorization: `Bearer ${X_API_KEY}`,
+      },
+    });
 
-  const routes = [
-    '/api/rankings?cache=false',
-    '/api/rankings/1?cache=false',
-    '/api/rankings?current_page=1&per_page=100&cache=false',
-    '/api/meets?cache=false',
-    '/api/meets?current_page=1&per_page=100&cache=false',
-    'api/meets/ipf?cache=false',
-    'api/meets/ipf?year=2020&cache=false',
-    '/api/records?cache=false',
-    '/api/users/johnhaack?cache=false',
-    '/api/status?cache=false',
-    '/api/users?search=haack&cache=false',
-  ];
+    const routes = [
+      '/api/rankings?cache=false',
+      '/api/rankings/1?cache=false',
+      '/api/rankings?current_page=1&per_page=100&cache=false',
+      '/api/meets?cache=false',
+      '/api/meets?current_page=1&per_page=100&cache=false',
+      'api/meets/ipf?cache=false',
+      'api/meets/ipf?year=2020&cache=false',
+      '/api/records?cache=false',
+      '/api/users/johnhaack?cache=false',
+      '/api/status?cache=false',
+      '/api/users?search=haack&cache=false',
+    ];
 
-  const promises = await Promise.allSettled(routes.map((r) => fetch(r)));
+    const promises = await Promise.allSettled(routes.map((r) => fetch(r)));
 
-  const data = promises.map((p) => {
-    const fulfilled = p.status === 'fulfilled';
-    const config = fulfilled ? p.value?.config : p.reason?.config;
-    const headers = fulfilled ? p.value?.headers : p.reason?.response?.headers;
+    const data = promises.map((p) => {
+      const fulfilled = p.status === 'fulfilled';
+      const config = fulfilled ? p.value?.config : p.reason?.config;
+      const headers = fulfilled ? p.value?.headers : p.reason?.response?.headers;
 
-    return {
-      status: fulfilled,
-      method: config?.method?.toUpperCase(),
-      url: config?.url,
-      date: headers?.date,
-    };
-  });
+      return {
+        status: fulfilled,
+        method: config?.method?.toUpperCase(),
+        url: config?.url,
+        date: headers?.date,
+      };
+    });
+
+    return data;
+  };
+
+  const cacheKey = `close-powerlifting-gloabl-status-call-cache`;
+
+  // @ts-ignore
+  let data = JSON.parse(await redis.get(cacheKey));
+
+  if (data === null) {
+    data = await fetchStatus();
+
+    // After fetching the data, store it in the Redis cache
+    // The 'EX' option sets an expiration time on the cache, in seconds
+    // Here we set it to 60 minutes (60 * 60 seconds)
+    // @ts-ignore
+    await redis.set(cacheKey, JSON.stringify(data), 'EX', 60 * 60);
+  }
 
   return data;
 }
