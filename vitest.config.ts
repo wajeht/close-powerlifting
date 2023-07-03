ce93a5265e2b26740b77a93f160226eee7231f68
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-02 20:19:39 -0500

    fet: test setup files and gloabl

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     testTimeout: 20000,
+    coverage: {
+      provider: 'c8',
+    },
+    setupFiles: ['./test-setup.ts'],
+    globals: true,
   },
 });
