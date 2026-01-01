473987f08d02fd40a4956401b701eff3d56200ad
wajeht <58354193+wajeht@users.noreply.github.com>
2026-01-01 02:02:45 -0600

    Remove deprecated configuration files and update email transport logic
    - Deleted obsolete configuration files for constants, email, and Swagger integration to streamline the codebase.
    - Updated email transport logic to utilize the centralized configuration module, enhancing maintainability and consistency across the application.

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,7 +1,14 @@
 import nodemailer from "nodemailer";
 
-import emailConfig from "../config/mail.config";
+import { config } from "../config";
 
-const mail = nodemailer.createTransport(emailConfig);
+const mail = nodemailer.createTransport({
+  host: config.email.host,
+  port: config.email.port,
+  auth: {
+    user: config.email.user,
+    pass: config.email.password,
+  },
+});
 
 export default mail;
